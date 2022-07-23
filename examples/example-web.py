128cf5f146dd8ca1c1b585396de654a274f1c9cb
Efe Akaröz <69296379+efeakaroz13@users.noreply.github.com>
2022-07-23 15:40:48 +0300

    Example change password command

--- examples/example-web.py
+++ examples/example-web.py
@@ -1,15 +1,17 @@
 import requests
 import json
 #Register
-page1_register = requests.post("http://192.168.1.39:1371/register?project=yourprojectname",data={"username":"testuser","password":"testpassword","fullname":"John Doe"})
+page1_register = requests.post("http://192.168.1.50:1371/register?project=yourprojectname",data={"username":"testuser","password":"testpassword","fullname":"John Doe"})
 #Example out: {'SCC': True, 'err': '', 'project': 'yourprojectname'}
 
 #Login
 #Using POST
-page2_login = requests.post("http://192.168.1.39:1371/login?project=yourprojectname",data={"username":"testuser","password":"testpassword"})
+page2_login = requests.post("http://192.168.1.50:1371/login?project=yourprojectname",data={"username":"testuser","password":"testpassword"})
 #Example out {'SCC': True, 'err': null, 'project': 'yourprojectname'}
 
 #Using get
-page3_login_get = requests.get("http://192.168.1.39:1371/login?project=yourprojectname&username=testuser&password=testpassword")
+page3_login_get = requests.get("http://192.168.1.50:1371/login?project=yourprojectname&username=testuser&password=testpassword")
 #Example out {'SCC': True, 'err': null, 'project': 'yourprojectname'}
 
+#Change password 
+page4_change_password= requests.get("http://192.168.1.50:1371/change_password?project=yourprojectname&username=testuser&oldpassword=testpassword&newpassword=checkoutmynewpassword")
